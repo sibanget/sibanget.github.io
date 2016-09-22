ba338447b7ccd232904730edcaa0ae342746e9be
Naufal <bosnaufalemail@gmail.com>
2016-09-22 20:50:25 +0700

    Add Sisa Hari dan Status Tugas

--- src/js/components/PostList/index.vue
+++ src/js/components/PostList/index.vue
@@ -4,12 +4,15 @@
   <div class="PostList">
     <loading-state v-if="items.length === 0" v-for="i in [1,2,3]"></loading-state>
 
-    <div v-if="items.length > 0" v-for="item in items | filterBy searchQuery | orderBy 'id' -1" class="PostList-item">
+    <div v-if="items.length > 0" v-for="item in items | filterBy searchQuery | orderBy 'id' -1" class="PostList-item {{ item.status }}">
       <div class="PostList-item_thumbnail">
         <a v-link="{ path: '/post/' + item.id }">
           <img :src="item.cover" alt="">
         </a>
-        <a class="PostList-item_label" @click.prevent="filterPost(item.category)">{{ item.category }}</a>
+        <div class="PostList-item_label">
+          <a class="PostList-item_label_item" @click.prevent="filterPost(item.category)">{{ item.category }}</a>
+          <a class="PostList-item_label_item PostList-item_label_item_sisa {{ item.status }}" @click.prevent="">{{ item.status === 'danger' ? 'BESOK!' : item.sisaHari + ' Hari Lagi' }}</a>
+        </div>
       </div>
       <div class="PostList-item_body">
         <h2 class="PostList-item_title"><a v-link="{ path: '/post/' + item.id }">{{ item.title }}</a></h2>
--- src/js/vuex/store/post.js
+++ src/js/vuex/store/post.js
@@ -9,6 +9,37 @@ const state = {
 const mutations = {
 
   POST_LIST_UPDATE_ALL (state,arg) {
+
+    function parseTanggal(str) {
+      let day = str.substr(0,2)
+      let month = str.substr(3,2)
+      let year = str.substr(6,4)
+      return [day,month,year]
+    }
+
+    function toDate(str) {
+      let parsedDate = parseTanggal(str)
+      return new Date(parsedDate[2], parsedDate[1] - 1, parsedDate[0])
+    }
+
+    function toBulanIndo(index) {
+      let bulans = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
+      return bulans[index]
+    }
+
+    function toTanggalIndo(date) {
+
+    }
+
+    arg.forEach((post) => {
+      let { date, deadline } = post
+      let dateNow = new Date()
+      let dateDeadline = toDate(deadline)
+      let selisihMiliSecond = dateDeadline - dateNow
+      let sisaHari = Math.ceil(selisihMiliSecond / 1000 / 60 / 60 / 24)
+      post.sisaHari = sisaHari
+      post.status = sisaHari === 1 ? 'danger' : sisaHari <= 2 ? 'warning' : 'aman'
+    })
     state.list = arg
   },
 
